47a89279d7d494d4f3174e8d517e3485f7d192b5
MaxMello <max.mails@ymail.com>
2017-03-14 22:36:13 +0100

    Custom Error Pages

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from MTShowcase import names
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
 class HomeView(TemplateView):
@@ -43,3 +45,17 @@ class About(TemplateView):
 
     def get(self, request, *args, **kwargs):
         return render(request, template_name=self.template_name, context={'names': names})
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
